32e72640763106f7a3f225ed5ad8f5884ad9d27a
Marcos di Silva <mdsmus@gmail.com>
2009-04-27 07:14:49 -0300

    Adapta estrutura do artigo para anppom2009

---
+++ make-anppom2009.mk
@@ -0,0 +1,15 @@
+NAME = anppom2009
+TEXSRCS = body-en.tex bibliography.tex abstract.tex resumo.tex
+OTHER += $(LILY_PDF) $(SVG_PDF) $(GNUPLOT_PDF)
+
+USE_PDFLATEX = 1
+
+include ~/.latexmk
+
+clean-partial:
+	rm -f $(NAME).pdf
+	rm -f $(NAME).aux
+	rm -f $(NAME).log
+	rm -f $(NAME).bbl
+	rm -f $(NAME).blg
+	rm -f $(NAME).out
